b3e6e2f2e1a475579723d82fa62a27f0d17a2ef7
emanuelnovakoski <emanuelnovakoski@hotmail.com>
2022-11-23 11:35:57 -0300

    implemented immediate printing on memory interface

--- src/interface/memoryInterface.cpp
+++ src/interface/memoryInterface.cpp
@@ -1,6 +1,5 @@
 #include "memoryInterface.hpp"
-#define WINDOW_WIDTH 40
-#define MAX_WINDOWS 2
+
 
 using namespace std;
 namespace NB32
@@ -13,7 +12,7 @@ namespace NB32
 		if (MemoryInterface::interfaceCounter > MAX_WINDOWS)
 			throw "Max memory windows exceeded";
 		
-		int xpos = COLS - MemoryInterface::interfaceCounter*40;
+		int xpos = COLS - MemoryInterface::interfaceCounter*WINDOW_WIDTH;
 		this->window = newwin(LINES, WINDOW_WIDTH, 0, xpos);
 		
 		box(this->window, 0 , 0);	
@@ -63,31 +62,56 @@ namespace NB32
 		memSegment = this->ram->read(topElement, LINES-2);
 		
 		
-		for (int i=0; i<memSegment.size(); i++)
+		for (int i=0; i+3<memSegment.size(); i+=4)
 		{
 			to_string(topElement+i);
 			
+			// build address
 			std::stringstream ss;
 			ss << std::hex << std::setfill('0') << std::setw(8) << topElement+i;
 			string index = ss.str();
 			
 			
-			string dword;
-			dword += memSegment[i];
-			dword += memSegment[i];
-			dword += memSegment[i];
-			dword += memSegment[i];
-	
+			// build dword as string
+			string d(" ");
+			d = d + memSegment[topElement+i] + " ";
+			d = d + memSegment[topElement+i+1] + " ";
+			d = d + memSegment[topElement+i+2] + " ";
+			d = d + memSegment[topElement+i+3];
 			
-			mvwprintw(this->window, i+1, 1, (index + ": " + memSegment[i]).c_str());
+			// TODO change this to deduce from string d
+			// build dword as number
+			string iChar(d);
+			iChar.erase (std::remove (iChar.begin(), iChar.end(), ' '), iChar.end());
+
+			long iDecimal = stol(iChar, nullptr, 2);
 			
+			// print address
+			wattron(this->window, A_BOLD);
+			wattron(this->window, A_STANDOUT);
+			wattron(this->window, A_UNDERLINE);
+			mvwprintw(this->window, i+1, 1, (index + ":").c_str());
+			wattroff(this->window, A_BOLD);
+			wattroff(this->window, A_STANDOUT);
+			wattroff(this->window, A_UNDERLINE);
+			
+			// print binary value of dword
+			wprintw(this->window, d.c_str());
+			
+			// print hexadecimal value of dword
+			mvwprintw(this->window, i+3, 3, "Immediate: ");
+			mvwprintw(this->window, i+3, IMM_HEX_XPOS, "0x");
+			wprintw(this->window, "%08lx ", iDecimal);
+			
+			// print decimal value of dwod
+			wmove(this->window, i+3, IMM_DEC_XPOS);
+			wprintw(this->window, "%012li ", iDecimal);
 			
 		}
 		
 		
-	
 		wrefresh(this->window);
-
+		
 	}
 	
 	void MemoryInterface::destroyScreen()
--- src/interface/memoryInterface.hpp
+++ src/interface/memoryInterface.hpp
@@ -1,6 +1,13 @@
 #include <ncurses.h>
 #include <iomanip>
+#include <algorithm>
 #include "../processor/memory.hpp"
+#define WINDOW_WIDTH 47
+#define MAX_WINDOWS 2
+#define IMM_HEX_XPOS 33
+#define IMM_DEC_XPOS 19
+
+// TODO decouple this class from the actual processor implementation
 
 namespace NB32
 {
@@ -9,7 +16,7 @@ namespace NB32
 		private:
 			WINDOW* window;
 			Memory* ram;
-			int memoryPointer;
+			int memoryPointer; // TODO change this to PC
 			static int interfaceCounter;
 			
 		public:
